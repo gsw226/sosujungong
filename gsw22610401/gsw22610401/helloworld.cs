c4c9d568abe6bd11ae4166f7651f66200a1c7e8a
gsw226 <156498620+gsw226@users.noreply.github.com>
2025-05-29 17:20:40 +0900

    --2

--- gsw22610401/gsw22610401/helloworld.cs
+++ gsw22610401/gsw22610401/helloworld.cs
@@ -10,7 +10,7 @@ namespace gsw22610401
     internal class helloworld
     {
         static int[] stk = new int[11];
-        int top = -1;
+        static int top = -1;
 
         public static void Main(string[] args)
         {
@@ -19,34 +19,60 @@ namespace gsw22610401
             while(true)
             {
                 Console.WriteLine("동작 입력: (1번 push, 2번 pop, 3번 종료)");
-                int num = Console.Read();
+                int num = int.Parse(Console.ReadLine());
 
                 switch (num)
                 {
                     case 1:
                         Console.WriteLine("저장할 수 입력");
-                        int pNum = Console.ReadLine();
-                        push(pNum);
+                        int pNum = int.Parse(Console.ReadLine());
+                        if (Push(pNum))
+                        {
+                            Console.WriteLine("저장 완료");
+                        }
+                        else
+                        {
+                            Console.WriteLine("스택이 가득 차서 저장 불가.");
+                        }
                         break;
-
                     case 2:
-                        Console.WriteLine(pop());
+                        int temp = Pop();
+                        if(temp == -2147483648)
+                        {
+                            Console.WriteLine("스택이 비어있음");    
+                        }
+                        else
+                        {
+                            Console.WriteLine(temp);
+                        }
                         break;
-
                     case 3:
-                        break;
+                        return;
                 }
-
-                //foreach(int a in stk)
-                //{
-                //    Console.WriteLine(a);
-                //}
+            }
+        }
+        static bool Push(int num)
+        {
+            if (top >= 9)
+            {
+                return false;
+            }
+            else
+            {
+                stk[++top] = num;
+                return true;
+            }
+        }
+        static int Pop()
+        {
+            if(top < 0)
+            {
+                return -2147483648;
+            }
+            else
+            {
+                return stk[top--];
             }
         }
     }
-
-
-
-
-
 }
