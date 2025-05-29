b6fc1019bc3ab593e5f5a94f3c664d212d2359a8
gsw226 <156498620+gsw226@users.noreply.github.com>
2025-05-29 16:42:59 +0900

    05.29

--- gsw22610401/gsw22610401/helloworld.cs
+++ gsw22610401/gsw22610401/helloworld.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,44 @@ namespace gsw22610401
 {
     internal class helloworld
     {
+        static int[] stk = new int[11];
+        int top = -1;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("스택 저장 프로그램");
+            
+            while(true)
+            {
+                Console.WriteLine("동작 입력: (1번 push, 2번 pop, 3번 종료)");
+                int num = Console.Read();
+
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine("저장할 수 입력");
+                        int pNum = Console.ReadLine();
+                        push(pNum);
+                        break;
+
+                    case 2:
+                        Console.WriteLine(pop());
+                        break;
+
+                    case 3:
+                        break;
+                }
+
+                //foreach(int a in stk)
+                //{
+                //    Console.WriteLine(a);
+                //}
+            }
+        }
     }
+
+
+
+
+
 }
